01a6b5948dc882e74e999d9b32ebae112c8ea64c
MAKSYM MONASTYRSKYY <t-systems.monastyrskyy@extaccount.com>
2024-12-02 14:21:28 +0100

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,10 +4,10 @@ import datetime as dt
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="* * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info(f'Python timer trigger function executed, at this time: {dt.datetime.now()}')
+    logging.info(f'Speed!!!!!!! Python timer trigger function executed, at this time: {dt.datetime.now()}')
