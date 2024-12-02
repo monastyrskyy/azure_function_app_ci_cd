aa27645d0269086c686eb9e77ea9ed2c8265857e
MAKSYM MONASTYRSKYY <t-systems.monastyrskyy@extaccount.com>
2024-12-02 16:12:34 +0100

    yet another try.
    swapped back, lets see if this now works.

--- function_app.py
+++ function_app.py
@@ -8,7 +8,7 @@ BERLIN_TZ = pytz.timezone("Europe/Berlin")
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+@app.timer_trigger(schedule="* * * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def timer_trigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
