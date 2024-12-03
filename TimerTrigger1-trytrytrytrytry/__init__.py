42d6baafaaa3f2d3d66515f1b47ea084678938ec
MAKSYM MONASTYRSKYY <t-systems.monastyrskyy@extaccount.com>
2024-12-03 19:58:46 +0100

    Update __init__.py

--- TimerTrigger1-trytrytrytrytry/__init__.py
+++ TimerTrigger1-trytrytrytrytry/__init__.py
@@ -5,10 +5,14 @@ import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
+    # Define the Berlin timezone
+    berlin_tz = pytz.timezone('Europe/Berlin')
+
+    # Get the current UTC time and convert it to Berlin time
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc)
+    berlin_timestamp = utc_timestamp.astimezone(berlin_tz).isoformat()
 
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('WWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!! Python timer trigger function ran at %s', utc_timestamp)
+    logging.info(f'WWWWWWWWWOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!!!! Python timer trigger function ran at {berlin_timestamp} (Berlin time)')
